b6b34f6a043761b31a94acffe6a6df959b140788
yosef <yosef.eldesoky@gmail.com>
2024-06-23 17:06:53 +0700

    fixed registeration bug
    Signed-off-by: yosef <yosef.eldesoky@gmail.com>

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('last_name');
-            $table->string('email')->unique();;
+            $table->string('email')->unique();
+            $table->string('phone_number');
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
