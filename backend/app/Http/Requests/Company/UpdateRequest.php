0c60d07017e3fa094654fb5b1397a20f221e499d
asror.tuhtamishov <asror.tuhtamishov@yandex.ru>
2024-07-10 10:35:50 +0000

    Revert "Merge branch 'STA-1791' into 'dev'"
    This reverts merge request !65

--- backend/app/Http/Requests/Company/UpdateRequest.php
+++ backend/app/Http/Requests/Company/UpdateRequest.php
@@ -23,7 +23,7 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'logoSrc' => 'nullable|string|max:255',
+            'logoSrc' => 'nullable|string|max:15',
             'currency_name' => 'required|string|max:255',
             'description' => 'required|string',
         ];
